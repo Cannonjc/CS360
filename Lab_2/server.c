3d84f4162e6dad0d7ddb65df708b0f674a8a7b86
Cannon Collins <cannoncollins@Cannons-MacBook-Pro.local>
2016-01-28 18:58:46 -0700

    If this doesnt work, the version before does

--- Lab_2/server.c
+++ Lab_2/server.c
@@ -20,8 +20,15 @@
 using namespace std;
 
 
+
+//------------------------------------------------------------------------------------
+
+
+
+// Determine if the character is whitespace
 bool isWhitespace(char c)
-{ switch (c)
+{
+    switch (c)
     {
         case '\r':
         case '\n':
@@ -54,8 +61,15 @@ char * GetLine(int fds)
     int amtread = 0;
     while((amtread = read(fds, tline + messagesize, 1)) < MAX_MSG_SZ)
     {
-        if (amtread >= 0)
+
+        if (amtread > 0)
+        {
             messagesize += amtread;
+        }
+        else if( amtread == 0 )
+        {
+            break;
+        }
         else
         {
             perror("Socket Error is:");
@@ -64,7 +78,9 @@ char * GetLine(int fds)
         }
         //fprintf(stderr,"%d[%c]", messagesize,message[messagesize-1]);
         if (tline[messagesize - 1] == '\n')
+        {
             break;
+        }
     }
     tline[messagesize] = '\0';
     chomp(tline);
@@ -73,7 +89,7 @@ char * GetLine(int fds)
     //fprintf(stderr, "GetLine: [%s]\n", line);
     return line;
 }
-
+    
 // Change to upper case and replace with underlines for CGI scripts
 void UpcaseAndReplaceDashWithUnderline(char *str)
 {
@@ -96,10 +112,10 @@ void UpcaseAndReplaceDashWithUnderline(char *str)
 // When calling CGI scripts, you will have to convert header strings
 // before inserting them into the environment.  This routine does most
 // of the conversion
-char *FormatHeader(char *str, const char *prefix)
+char *FormatHeader(char *str, char *prefix)
 {
     char *result = (char *)malloc(strlen(str) + strlen(prefix));
-    char* value = strchr(str,':') + 1;
+    char* value = strchr(str,':') + 2;
     UpcaseAndReplaceDashWithUnderline(str);
     *(strchr(str,':')) = '\0';
     sprintf(result, "%s%s=%s", prefix, str, value);
@@ -112,6 +128,7 @@ char *FormatHeader(char *str, const char *prefix)
 
 void GetHeaderLines(std::vector<char *> &headerLines, int skt, bool envformat)
 {
+
     // Read the headers, look for specific ones that may change our responseCode
     char *line;
     char *tline;
@@ -119,25 +136,25 @@ void GetHeaderLines(std::vector<char *> &headerLines, int skt, bool envformat)
     tline = GetLine(skt);
     while(strlen(tline) != 0)
     {
-        if (strstr(tline, "Content-Length") ||
+        if (strstr(tline, "Content-Length") || 
             strstr(tline, "Content-Type"))
         {
             if (envformat)
-                line = FormatHeader(tline, "");
+                line = FormatHeader(tline, const_cast<char*>( "" ) );
             else
                 line = strdup(tline);
         }
         else
         {
             if (envformat)
-                line = FormatHeader(tline, "HTTP_");
+                line = FormatHeader(tline, const_cast<char*>( "HTTP_" ) );
             else
             {
                 line = (char *)malloc((strlen(tline) + 10) * sizeof(char));
-                sprintf(line, "HTTP_%s", tline);
+                sprintf(line, "HTTP_%s", tline);                
             }
         }
-        //fprintf(stderr, "Header --> [%s]\n", line);
+        fprintf(stderr, "Header --> [%s]\n", line);
         
         headerLines.push_back(line);
         free(tline);
@@ -145,6 +162,137 @@ void GetHeaderLines(std::vector<char *> &headerLines, int skt, bool envformat)
     }
     free(tline);
 }
+
+
+
+//-------------------------------------------------------------------------------------
+
+
+// bool isWhitespace(char c)
+// { switch (c)
+//     {
+//         case '\r':
+//         case '\n':
+//         case ' ':
+//         case '\0':
+//             return true;
+//         default:
+//             return false;
+//     }
+// }
+
+// // Strip off whitespace characters from the end of the line
+// void chomp(char *line)
+// {
+//     int len = strlen(line);
+//     while (isWhitespace(line[len]))
+//     {
+//         line[len--] = '\0';
+//     }
+// }
+
+// // Read the line one character at a time, looking for the CR
+// // You dont want to read too far, or you will mess up the content
+// char * GetLine(int fds)
+// {
+//     char tline[MAX_MSG_SZ];
+//     char *line;
+    
+//     int messagesize = 0;
+//     int amtread = 0;
+//     while((amtread = read(fds, tline + messagesize, 1)) < MAX_MSG_SZ)
+//     {
+//         if (amtread >= 0)
+//             messagesize += amtread;
+//         else
+//         {
+//             perror("Socket Error is:");
+//             fprintf(stderr, "Read Failed on file descriptor %d messagesize = %d\n", fds, messagesize);
+//             exit(2);
+//         }
+//         //fprintf(stderr,"%d[%c]", messagesize,message[messagesize-1]);
+//         if (tline[messagesize - 1] == '\n')
+//             break;
+//     }
+//     tline[messagesize] = '\0';
+//     chomp(tline);
+//     line = (char *)malloc((strlen(tline) + 1) * sizeof(char));
+//     strcpy(line, tline);
+//     //fprintf(stderr, "GetLine: [%s]\n", line);
+//     return line;
+// }
+
+// // Change to upper case and replace with underlines for CGI scripts
+// void UpcaseAndReplaceDashWithUnderline(char *str)
+// {
+//     int i;
+//     char *s;
+    
+//     s = str;
+//     for (i = 0; s[i] != ':'; i++)
+//     {
+//         if (s[i] >= 'a' && s[i] <= 'z')
+//             s[i] = 'A' + (s[i] - 'a');
+        
+//         if (s[i] == '-')
+//             s[i] = '_';
+//     }
+    
+// }
+
+
+// // When calling CGI scripts, you will have to convert header strings
+// // before inserting them into the environment.  This routine does most
+// // of the conversion
+// char *FormatHeader(char *str, const char *prefix)
+// {
+//     char *result = (char *)malloc(strlen(str) + strlen(prefix));
+//     char* value = strchr(str,':') + 1;
+//     UpcaseAndReplaceDashWithUnderline(str);
+//     *(strchr(str,':')) = '\0';
+//     sprintf(result, "%s%s=%s", prefix, str, value);
+//     return result;
+// }
+
+// // Get the header lines from a socket
+// //   envformat = true when getting a request from a web client
+// //   envformat = false when getting lines from a CGI program
+
+// void GetHeaderLines(std::vector<char *> &headerLines, int skt, bool envformat)
+// {
+//     // Read the headers, look for specific ones that may change our responseCode
+//     char *line;
+//     char *tline;
+    
+//     tline = GetLine(skt);
+//     while(strlen(tline) != 0)
+//     {
+//         if (strstr(tline, "Content-Length") ||
+//             strstr(tline, "Content-Type"))
+//         {
+//             if (envformat)
+//                 line = FormatHeader(tline, "");
+//             else
+//                 line = strdup(tline);
+//         }
+//         else
+//         {
+//             if (envformat)
+//                 line = FormatHeader(tline, "HTTP_");
+//             else
+//             {
+//                 line = (char *)malloc((strlen(tline) + 10) * sizeof(char));
+//                 sprintf(line, "HTTP_%s", tline);
+//             }
+//         }
+//         //fprintf(stderr, "Header --> [%s]\n", line);
+        
+//         headerLines.push_back(line);
+//         free(tline);
+//         tline = GetLine(skt);
+//     }
+//     free(tline);
+// }
 
 string getFileName(char *message)
 {
