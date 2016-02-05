2f182509aadb9c0f01f6190e12edb59cee4c7160
Cannon Collins <cannoncollins@Cannons-MacBook-Pro.local>
2016-02-05 02:30:50 -0700

    lab 3 cont.

--- Lab_3/threads.cpp
+++ Lab_3/threads.cpp
@@ -379,6 +379,7 @@ int main (int argc, char *argv[])
    }
 
     printf("\nStarting server");
+    printf("Prefix is: %s\n", prefix);
 
     //put the code from sigint right here to handle the constant refresh(sig pipe is really only on we need)
 
