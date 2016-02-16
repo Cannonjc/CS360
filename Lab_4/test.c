822a603791ff01e1249eea888dcb2d9e71371053
Cannon Collins <cannoncollins@Cannons-MacBook-Pro.local>
2016-02-16 13:46:49 -0700

    Lab4 work

--- Lab_4/test.c
+++ Lab_4/test.c
@@ -115,7 +115,7 @@ int  main(int argc, char* argv[])
 			return 0;
 		}
 		std::string tempRequest = "GET " + page + " HTTP/1.0\r\n\r\n";
-		char request[tempRequest.sizeof()];
+		char request[tempRequest.size()];
 		strcpy(request,tempRequest.c_str());
 
 	    write(hSocket[i],request,strlen(request));
