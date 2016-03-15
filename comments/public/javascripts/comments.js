e6f9396e2924466b0067f68efc9cb678c788357c
Cannon Collins <cannoncollins@Cannons-MacBook-Pro.local>
2016-03-14 18:02:41 -0600

    added functionality to site

--- comments/public/javascripts/comments.js
+++ comments/public/javascripts/comments.js
@@ -19,9 +19,18 @@ $(document).ready(function(){
       $.getJSON('comments', function(data) {
         console.log(data);
         var everything = "<ul>";
-        for(var comment in data) {
+        if ($(#Password).val() == "admin") {
+          $("#json").text("Please enter the correct password to get the accurate results");
+          for(var comment in data) {
           com = data[comment];
           everything += "<li>Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+          }
+        }
+        else {
+          for(var comment in data) {
+          com = data[comment];
+          everything += "<li>Name: " + com.Name.split("").reverse().join(""); + " -- Comment: " + com.Comment.split("").reverse().join(""); + "</li>";
+          }
         }
         everything += "</ul>";
         $("#comments").html(everything);
