eaa00f59bf01169396b575034e573d3a8c151831
Cannon Collins <cannoncollins@Cannons-MacBook-Pro.local>
2016-03-27 23:15:03 -0600

    Start of start ups

---
+++ Lab_9/public/javascripts/app.js
@@ -0,0 +1,17 @@
+angular.module('comment', [])
+  .controller('MainCtrl', [
+    '$scope',
+    function($scope) {
+      $scope.test = 'Hello world!';
+
+      $scope.comments = [
+      {title:'Comment 1', upvotes:5},
+      {title:'Comment 2', upvotes:6},
+      {title:'Comment 3', upvotes:1},
+      {title:'Comment 4', upvotes:4},
+      {title:'Comment 5', upvotes:3}
+      ];
+
+
+    }
+  ]);
