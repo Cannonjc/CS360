623a1c6384e7bc4be294558d6f5686e27b721161
Cannon Collins <cannoncollins@Cannons-MacBook-Pro.local>
2016-04-04 16:51:57 -0600

    get and post for comments

--- Lab_9/models/Comments.js
+++ Lab_9/models/Comments.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 var CommentSchema = new mongoose.Schema({
   title: String,
-  upvotes: {type: Number, default: 0},
+  upvotes: {
+    type: Number,
+    default: 0
+  },
 });
 mongoose.model('Comment', CommentSchema);
